83e3e3659c34a5206271c65b3dd151ffed67c1ec
hadzhiDev <hm03.business@gmail.com>
2026-08-13 17:37:35 +0600

    Allowed hosts set public

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fcs-6&47%$fautch#nket9f0m(ya7halnao3!6jk9w3!7#_h1l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
